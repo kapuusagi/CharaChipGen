dbf0f44681fd5f0e6b6337e69e31012648d000e2
usagizinrui <usagizinrui@yahoo.co.jp>
2020-09-21 18:22:35 +0900

    端っこ丸の矩形を作成するツールを用意した。

---
+++ RoundRect/FormMain.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace RoundRect
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownHeight = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownOutline = new System.Windows.Forms.NumericUpDown();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCopyTo = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOutline)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.buttonClose);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 244);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(544, 29);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(466, 3);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.OnButtonClose);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.flowLayoutPanel2);
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(401, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(6);
+            this.panel1.Size = new System.Drawing.Size(143, 244);
+            this.panel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDownWidth, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDownHeight, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDownOutline, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelColor, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 6);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(131, 148);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Outline";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Color";
+            // 
+            // numericUpDownWidth
+            // 
+            this.numericUpDownWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownWidth.Location = new System.Drawing.Point(50, 9);
+            this.numericUpDownWidth.Maximum = new decimal(new int[] {
+            1200,
+            0,
+            0,
+            0});
+            this.numericUpDownWidth.Name = "numericUpDownWidth";
+            this.numericUpDownWidth.Size = new System.Drawing.Size(78, 19);
+            this.numericUpDownWidth.TabIndex = 4;
+            this.numericUpDownWidth.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownHeight
+            // 
+            this.numericUpDownHeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownHeight.Location = new System.Drawing.Point(50, 46);
+            this.numericUpDownHeight.Maximum = new decimal(new int[] {
+            1200,
+            0,
+            0,
+            0});
+            this.numericUpDownHeight.Name = "numericUpDownHeight";
+            this.numericUpDownHeight.Size = new System.Drawing.Size(78, 19);
+            this.numericUpDownHeight.TabIndex = 5;
+            this.numericUpDownHeight.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownOutline
+            // 
+            this.numericUpDownOutline.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownOutline.Location = new System.Drawing.Point(50, 83);
+            this.numericUpDownOutline.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.numericUpDownOutline.Name = "numericUpDownOutline";
+            this.numericUpDownOutline.Size = new System.Drawing.Size(78, 19);
+            this.numericUpDownOutline.TabIndex = 6;
+            this.numericUpDownOutline.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDownOutline.ValueChanged += new System.EventHandler(this.OnSizeChanged);
+            // 
+            // labelColor
+            // 
+            this.labelColor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelColor.BackColor = System.Drawing.Color.Black;
+            this.labelColor.Location = new System.Drawing.Point(50, 111);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(78, 37);
+            this.labelColor.TabIndex = 7;
+            this.labelColor.Click += new System.EventHandler(this.OnLabelColorClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(6);
+            this.panel2.Size = new System.Drawing.Size(401, 244);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(389, 232);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.buttonSave);
+            this.flowLayoutPanel2.Controls.Add(this.buttonCopyTo);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(6, 154);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(131, 100);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(3, 3);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(122, 23);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.OnButtonSaveClick);
+            // 
+            // buttonCopyTo
+            // 
+            this.buttonCopyTo.Location = new System.Drawing.Point(3, 32);
+            this.buttonCopyTo.Name = "buttonCopyTo";
+            this.buttonCopyTo.Size = new System.Drawing.Size(122, 23);
+            this.buttonCopyTo.TabIndex = 1;
+            this.buttonCopyTo.Text = "Copy to Clipboard";
+            this.buttonCopyTo.UseVisualStyleBackColor = true;
+            this.buttonCopyTo.Click += new System.EventHandler(this.OnButtonCopyToCliboardClick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 273);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "FormMain";
+            this.Text = "RoundRect";
+            this.Shown += new System.EventHandler(this.OnShown);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOutline)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownWidth;
+        private System.Windows.Forms.NumericUpDown numericUpDownHeight;
+        private System.Windows.Forms.NumericUpDown numericUpDownOutline;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCopyTo;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ RoundRect/FormMain.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CGenImaging;
+
+namespace RoundRect
+{
+    /// <summary>
+    /// Main画面
+    /// </summary>
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// 新しいインスタンスを構築する。
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ボタンをクローズされたときに通知を受け取る。
+        /// </summary>
+        /// <param name="sender">送信オブジェクト</param>
+        /// <param name="e">イベントオブジェクト</param>
+        private void OnButtonClose(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// カラーラベルがクリックされたときに通知を受け取る。
+        /// </summary>
+        /// <param name="sender">送信元オブジェクト</param>
+        /// <param name="e">イベントオブジェクト</param>
+        private void OnLabelColorClick(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog(this) != DialogResult.OK)
+            {
+                return;
+            }
+
+            labelColor.BackColor = colorDialog.Color;
+            try
+            {
+                UpdateImage();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+                MessageBox.Show(this, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// サイズが変更されたときの処理を行う。
+        /// </summary>
+        /// <param name="sender">送信元オブジェクト</param>
+        /// <param name="e">イベントオブジェクト</param>
+        private void OnSizeChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                UpdateImage();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+                MessageBox.Show(this, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 更新する。
+        /// </summary>
+        private void UpdateImage()
+        {
+            int fillAreaWidth = (int)(numericUpDownWidth.Value);
+            int fillAreaHeight = (int)(numericUpDownHeight.Value);
+            int outlineWidth = (int)(numericUpDownOutline.Value);
+            Color c = labelColor.BackColor;
+            int width = fillAreaWidth + outlineWidth * 2;
+            int height = fillAreaHeight + outlineWidth * 2;
+
+            if ((width > 0) || (height > 0))
+            {
+                ImageBuffer imageBuffer = ImageBuffer.Create(width, height);
+                PaintRoundRect(imageBuffer, outlineWidth, c);
+                pictureBox.Image = imageBuffer.GetImage();
+            }
+            else
+            {
+                pictureBox.Image = null;
+            }
+        }
+
+        /// <summary>
+        /// RoundRectを描画する。
+        /// </summary>
+        /// <param name="imageBuffer">イメージバッファ</param>
+        /// <param name="outlineWidth">アウトライン幅</param>
+        /// <param name="color">色</param>
+        private void PaintRoundRect(ImageBuffer imageBuffer, int outlineWidth, Color color)
+        {
+            Point leftTop = new Point(outlineWidth, outlineWidth);
+            Point rightTop = new Point(imageBuffer.Width - outlineWidth, outlineWidth);
+            Point leftBottom = new Point(outlineWidth, imageBuffer.Width - outlineWidth);
+            Point rightBottom = new Point(imageBuffer.Width - outlineWidth, imageBuffer.Height - outlineWidth);
+
+            for (int y = 0; y < outlineWidth; y++)
+            {
+                // 左上
+                for (int x = 0; x < outlineWidth; x++)
+                {
+                    int a = GetAlpha(leftTop.X, leftTop.Y, x, y, outlineWidth);
+                    Color c = Color.FromArgb(a, color);
+                    imageBuffer.SetPixel(x, y, c);
+                }
+                // 上端
+                int alphaY = GetAlpha(leftTop.Y, y, outlineWidth);
+                Color colorY = Color.FromArgb(alphaY, color);
+                for (int x = leftTop.X; x < rightTop.X; x++)
+                {
+                    imageBuffer.SetPixel(x, y, colorY);
+                }
+
+                // 右上
+                for (int x = rightTop.X; x < imageBuffer.Width; x++)
+                {
+                    int a = GetAlpha(rightTop.X, rightTop.Y, x, y, outlineWidth);
+                    Color c = Color.FromArgb(a, color);
+                    imageBuffer.SetPixel(x, y, c);
+                }
+            }
+
+            for (int y = leftTop.Y; y < leftBottom.Y; y++)
+            {
+                // 左端
+                for (int x = 0; x < leftTop.X; x++)
+                {
+                    int a = GetAlpha(leftTop.X, x, outlineWidth);
+                    Color c = Color.FromArgb(a, color);
+                    imageBuffer.SetPixel(x, y, c);
+                }
+
+                // 中央矩形
+                for (int x = leftTop.X; x < rightTop.X; x++)
+                {
+                    imageBuffer.SetPixel(x, y, color);
+                }
+                // 右端
+                for (int x = rightTop.X; x < imageBuffer.Width; x++)
+                {
+                    int a = GetAlpha(rightTop.X, x, outlineWidth);
+                    Color c = Color.FromArgb(a, color);
+                    imageBuffer.SetPixel(x, y, c);
+                }
+            }
+
+            for (int y = leftBottom.Y; y < imageBuffer.Height; y++)
+            {
+                // 左下
+                for (int x = 0; x < leftBottom.X; x++)
+                {
+                    int a = GetAlpha(leftBottom.X, leftBottom.Y, x, y, outlineWidth);
+                    Color c = Color.FromArgb(a, color);
+                    imageBuffer.SetPixel(x, y, c);
+                }
+
+                // 下端
+                int alphaY = GetAlpha(leftBottom.Y, y, outlineWidth);
+                Color colorY = Color.FromArgb(alphaY, color);
+                for (int x = leftBottom.X; x < rightBottom.X; x++)
+                {
+                    imageBuffer.SetPixel(x, y, colorY);
+                }
+
+                // 右下
+                for (int x = rightBottom.X; x < imageBuffer.Width; x++)
+                {
+                    int a = GetAlpha(rightBottom.X, rightBottom.Y, x, y, outlineWidth);
+                    Color c = Color.FromArgb(a, color);
+                    imageBuffer.SetPixel(x, y, c);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// アルファの値を得る。
+        /// </summary>
+        /// <param name="centerP">センター位置</param>
+        /// <param name="pos">X位置</param>
+        /// <param name="width">Y位置</param>
+        /// <returns>アルファの値</returns>
+        private int GetAlpha(int centerP, int pos, int width)
+        {
+            double r = Math.Abs((double)(pos - centerP) / (double)(width));
+            int a = (int)(Math.Round((1.0 - r) * 255.0));
+            return (a > 255) ? 255 : (a < 0) ? 0 : a;
+        }
+
+        /// <summary>
+        /// アルファの値を得る。
+        /// </summary>
+        /// <param name="centerX">中央X位置</param>
+        /// <param name="centerY">中央Y位置</param>
+        /// <param name="x">X位置</param>
+        /// <param name="y">Y位置</param>
+        /// <param name="width">幅</param>
+        /// <returns>アルファの値</returns>
+        private int GetAlpha(int centerX, int centerY, int x, int y, int width)
+        {
+            double dis = Math.Sqrt((centerX - x) * (centerX - x) + (centerY - y) * (centerY - y));
+            int a = (int)(Math.Round((1.0 - dis / width) * 255.0));
+            return (a > 255) ? 255 : (a < 0) ? 0 : a;
+        }
+
+        /// <summary>
+        /// フォームが表示されたときに通知を受け取る。
+        /// </summary>
+        /// <param name="sender">送信元オブジェクト</param>
+        /// <param name="e">イベントオブジェクト</param>
+        private void OnShown(object sender, EventArgs e)
+        {
+            try
+            {
+                UpdateImage();
+            }
+            catch
+            {
+                // do nothing.
+            }
+        }
+
+        /// <summary>
+        /// クリップボードにコピーが選択された。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnButtonCopyToCliboardClick(object sender, EventArgs e)
+        {
+            Image image = pictureBox.Image;
+            Clipboard.SetImage(image);
+        }
+
+        /// <summary>
+        /// Saveボタンがクリックされたときに通知を受け取る。
+        /// </summary>
+        /// <param name="sender">送信元オブジェクト</param>
+        /// <param name="e">イベントオブジェクト</param>
+        private void OnButtonSaveClick(object sender, EventArgs e)
+        {
+            try
+            {
+                Image image = pictureBox.Image;
+                if (image == null)
+                {
+                    return;
+                }
+                if (saveFileDialog.ShowDialog(this) != DialogResult.OK)
+                {
+                    return;
+                }
+                string path = saveFileDialog.FileName;
+                image.Save(path, System.Drawing.Imaging.ImageFormat.Png);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+                MessageBox.Show(this, ex.Message);
+            }
+        }
+    }
+}
