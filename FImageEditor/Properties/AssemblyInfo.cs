7a3cadd06c737f7a0048457be0d919a8a2b41a97
kapuusagi <usagizinrui@yahoo.co.jp>
2021-05-06 15:40:35 +0900

    [FixedBug]アセンブリ情報が正しくないので修正した。

--- FImageEditor/Properties/AssemblyInfo.cs
+++ FImageEditor/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FImageEditor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FImageEditor")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyCopyright("Copyright © kapuusagi Inc. 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
